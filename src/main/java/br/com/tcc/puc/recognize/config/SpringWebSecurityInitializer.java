b689e47baa7100b19bf18395a3338a3bf734e368
mmarcosa <marcos.barbosa8@fatec.sp.gov.br>
2020-07-19 22:43:51 -0300

    add começo de autenticação com springboot

---
+++ src/main/java/br/com/tcc/puc/recognize/config/SpringWebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package br.com.tcc.puc.recognize.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
